b12034c122ff8eecbc7cf42bcfeb79469cfa0384
Leonid Shalupov <leonid@shalupov.com>
2015-11-30 11:30:39 +0100

    update Git to 2.6.3

--- Setup/DownloadExternals.cmd
+++ Setup/DownloadExternals.cmd
@@ -2,8 +2,8 @@
 
 REM Also update in Product.wxs and UI\RequiredSoftwareDlg.wxs
 
-IF NOT EXIST "%~p0\..\bin\Git-2.5.3-64-bit.exe" (
-    "%~p0\tools\curl.exe" -L -k -o ..\bin\Git-2.5.3-64-bit.exe https://github.com/git-for-windows/git/releases/download/v2.5.3.windows.1/Git-2.5.3-64-bit.exe
+IF NOT EXIST "%~p0\..\bin\Git-2.6.3-64-bit.exe" (
+    "%~p0\tools\curl.exe" -L -k -o ..\bin\Git-2.6.3-64-bit.exe https://github.com/git-for-windows/git/releases/download/v2.6.3.windows.1/Git-2.6.3-64-bit.exe
     IF ERRORLEVEL 1 EXIT /B 1
 )
 
